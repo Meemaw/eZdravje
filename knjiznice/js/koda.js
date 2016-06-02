2564571f1c119fbdc2682bfe3458531fd24130e2
Matej <totalfr33k@gmail.com>
2016-06-02 12:48:08 +0000

    Nice commit

--- knjiznice/js/koda.js
+++ knjiznice/js/koda.js
@@ -1,4 +1,4 @@
-
+"use strict";
 var baseUrl = 'https://rest.ehrscape.com/rest/v1';
 var queryUrl = baseUrl + '/query';
 
@@ -21,9 +21,61 @@ var kreiranjeUspelo = false;
 
 $(document).ready(function(){
     sectionButtonClick(0);
-    generirajUporabnike();
+    getPacients();
     drawVprasanja();
-}) ;
+});
+
+
+function zbrisi(pacient) {
+    console.log(pacient);
+    var id = pacient.partyId;
+    setSessionId();
+    $.ajax({
+        url: baseUrl + "/demographics/party/" + id,
+        type: 'DELETE',
+        success: function(res) {
+            console.log("Deleted pacient: " + res);
+            getPacients();
+        },
+	    error: function(err) {
+	        console.log(err);
+	    }
+        
+    });
+}
+
+
+function getPacients() {
+    setSessionId();
+    var searchData = [
+        {key: "app_id", value: "eZdravnikBase"},
+    ];
+    
+    $.ajax({
+        url: baseUrl + "/demographics/party/query",
+        type: 'POST',
+        contentType: 'application/json',
+        data: JSON.stringify(searchData),
+        success: function (res) {
+            myPatients = [];
+            drawPatients(myPatients);
+            if(!res) return;
+            for (var i in res.parties) {
+                var party = res.parties[i];
+                var ehrId;
+                for (var j in party.partyAdditionalInfo) {
+                    if (party.partyAdditionalInfo[j].key === 'ehrId') {
+                        ehrId = party.partyAdditionalInfo[j].value;
+                        break;
+                    }
+                }
+                var pacient = {ime: party.firstNames, priimek: party.lastNames, datumRojstva: party.dateOfBirth, id: ehrId, partyId: party.id};
+                myPatients.push(pacient);
+                drawPatients(myPatients);
+            }
+        }
+    });
+}
 
 function odgovoriClick(id) {
     var odgovor = $("#form" + id).val();
@@ -36,6 +88,81 @@ function odgovoriClick(id) {
         drawVprasanja();
     }
 
+}
+
+function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+
+function dodajVitalneZnakePacientu(ehrId) {
+    for(var i = 0; i < 4; i++) {
+        var today = new Date();
+        var visina = getRandomInt(160, 210);
+        var teza =   visina - (100);
+        var temperatura = getRandomInt(36,41);
+        var sys = getRandomInt(80,200);
+        var dis = getRandomInt(50,140);
+        var nasicenost = getRandomInt(30,99);
+        var merilec = "Jožica Božiza";
+        dodajMeritveVitalnihZnakov(ehrId,today,visina,teza,temperatura,sys,dis,nasicenost,merilec);
+    }
+}
+
+function dodajMeritveVitalnihZnakov(ehrId, datum, visina, teza, temperatura, sys, dis, nasicenost, merilec) {
+    if(!ehrId  || ehrId.trim().length == 0) return;
+    setSessionId();
+	var podatki = {
+		// Struktura predloge je na voljo na naslednjem spletnem naslovu:
+  // https://rest.ehrscape.com/rest/v1/template/Vital%20Signs/example
+	    "ctx/language": "en",
+	    "ctx/territory": "SI",
+	    "ctx/time": datum,
+	    "vital_signs/height_length/any_event/body_height_length": visina,
+	    "vital_signs/body_weight/any_event/body_weight": teza,
+	   	"vital_signs/body_temperature/any_event/temperature|magnitude": temperatura,
+	    "vital_signs/body_temperature/any_event/temperature|unit": "°C",
+	    "vital_signs/blood_pressure/any_event/systolic": sys,
+	    "vital_signs/blood_pressure/any_event/diastolic": dis,
+	    "vital_signs/indirect_oximetry:0/spo2|numerator": nasicenost
+	};
+	
+	var parametriZahteve = {
+	    ehrId: ehrId,
+	    templateId: 'Vital Signs',
+	    format: 'FLAT',
+	    committer: merilec
+	};
+	
+	$.ajax({
+	    url: baseUrl + "/composition?" + $.param(parametriZahteve),
+	    type: 'POST',
+	    contentType: 'application/json',
+	    data: JSON.stringify(podatki),
+	    success: function (res) {
+	        console.log("Uspešno dodani vitalni znaki");
+	    },
+	    error: function(err) {
+	        console.log("Error adding vital signs: " + JSON.parse(err.responseText).userMessage + "'!" );
+	    }
+	});
+}
+
+
+
+function pacientClick(pacient) {
+    $("#patientContainerWrap").hide();
+    $("#patient-add").hide();
+    $("#vprasanja").hide();
+    $("#master-detail").hide();
+    $("#patient-detail").show();
+    drawGeneralInfo(pacient);
+}
+
+function drawGeneralInfo(pacient) {
+    $("#pacient-name").text(pacient.ime + "  " + pacient.priimek);
+    $("#pacient-date").text("Datum rojstva:  " + pacient.datumRojstva);
+    $("#pacient-ehrid").text("ehrId pacienta:  " + pacient.id);
 }
 
 
@@ -95,12 +222,35 @@ function drawBrezVprasanj() {
 function drawVprasanje(vprasanje) {
     var vprasanjaDiv = $("#vprasanjaContainer");
     var pac = vprasanje.patient;
-    vprasanjaDiv.append('<div class="padding-small"><p><b>'+ pac.ime + " " + pac.priimek + '</b>:  ' + vprasanje.tekst + '</p><textarea class="form-control" id="form' + vprasanje.id + '" rows="3" placeholder="Vnesi odgovor"></textarea><button type="button" class="margin-small btn btn-success right-align" onclick="odgovoriClick(' + vprasanje.id + ')">Odgovori</button></div>');
+    vprasanjaDiv.append('<div class="padding-small"><p><b>'+ pac.ime + " " + pac.priimek + '</b>:  ' + vprasanje.tekst + '</p><textarea class="form-control" id="form' + vprasanje.id + '" rows="3" placeholder="Vnesi odgovor"></textarea><button type="button" class="margin-small btn btn-success right-alignn" onclick="odgovoriClick(' + vprasanje.id + ')">Odgovori</button></div>');
 }
 
+
+
+
 function drawPatients(patients) {
+    
+    function dodajClicke(el, patient) {
+        el.find("button").click(function(event) {
+                        event.preventDefault();
+                        zbrisi(patient);
+                        console.log("cupa");
+                        return false;
+        });
+        
+        el.click(function(event) {
+            event.preventDefault();
+            pacientClick(patient);
+            console.log("opa");
+            return false;
+        });
+    }
+    
+    console.log(patients.length);
     var patientMenu = $("#patientContainer");
     patientMenu.empty();
+    
+    
 
 
     for(var i = 0; i < patients.length / 3; i++) {
@@ -109,15 +259,27 @@ function drawPatients(patients) {
         patientMenu.append(row);
         var rowInsert = $("#row" + i);
         
-        for(var j = i*3; j < i*3+3; j++) {
+        for(var j = i*3; j < i*3+3 && j < patients.length; j++) {
             var patient = patients[j];
             var image =  '<img src="profile.png" class="img-responsive margin-center" alt="Cinque Terre" width="100" height="100">'
-            if(j % 3 == 0)
-                rowInsert.append('<div class="col-sm-3 btn btn-default">' + image + '<p class="strong-text">' + patient.ime + " " + patient.priimek + '<p> Datum rojstva: ' + patient.datumRojstva + '<p class="light-text">' + patient.id + '</p></p></p></div>');
-            else 
-                rowInsert.append('<div class="col-sm-3 col-md-offset-1 btn btn-default">' + image + '<p class="strong-text">' + patient.ime + " " + patient.priimek + '<p> Datum rojstva: ' + patient.datumRojstva +'<p class="light-text">' + patient.id + '</p></p></p></div>');
+            var divStart = '<div class="col-sm-3 btn btn-default';
+            var trash = '<button type="button" class="btn btn-default right-align" aria-label="Left Align"><span class="glyphicon glyphicon-trash" aria-hidden="true"></span></button>'
+            var podatki = '<p class="strong-text">' + patient.ime + " " + patient.priimek + '<p> Datum rojstva: ' + patient.datumRojstva + '<p class="light-text">' + patient.id + '</p></p></p></div>'
+
                 
+            var htmlLeft = divStart + '">' + trash + image + podatki;
+            var htmlRight = divStart + ' col-md-offset-1">' + trash + image+ podatki;
             
+            var left = jQuery(htmlLeft);
+            var right = jQuery(htmlRight);
+            dodajClicke(left, patient);
+            dodajClicke(right, patient);
+            
+            
+            if(j % 3 == 0)
+                rowInsert.append(left);
+            else 
+                rowInsert.append(right);
         }  
     }
     
@@ -141,15 +303,6 @@ function sectionButtonClick(st) {
 }
 
 
-function drawDropdownMenu(patients) {
-    var menu = $("ul.dropdown-menu");
-    menu.empty();
-    for(var i = 0; i < patients.length; i++) {
-        var patient = patients[i];
-        menu.append('<li><a href="#">' + patient.ime + " " + patient.priimek + '</a></li>');
-    }
-}
-
 
 function najdiPacientaByEHRid() {
     setSessionId();
@@ -164,7 +317,6 @@ function najdiPacientaByEHRid() {
 			type: 'GET',
 	    	success: function (data) {
 	    	    $("#preberiSporocilo").empty();
-	    	    console.log("success");
 	    	    var party = data.party;
 	    	    drawAlertDiv("success", "Dodan pacient: " + party.firstNames + " " + party.lastNames + ", rojen " + party.dateOfBirth);
 				var patient = {ime: party.firstNames, priimek: party.lastNames + ", rojen " + party.dateOfBirth, id: ehrId};
@@ -184,20 +336,28 @@ function najdiPacientaByEHRid() {
 
 function dodajanjePacientov() {
     $("#patientContainerWrap").hide();
-    $("#patient-add").show();
     $("#vprasanja").hide();
+    $("#patient-detail").hide();
+    $("#master-detail").show();
+    $("#patient-add").show();
+    
 }
 
 function mojiPacienti() {
-    $("#patientContainerWrap").show();
+    $("#patient-detail").hide();
     $("#patient-add").hide();
     $("#vprasanja").hide();
+    $("#master-detail").show();
+    $("#patientContainerWrap").show();
+    
 }
 
 function vprasanjaSection() {
+    $("#patient-detail").hide();
     $("#patientContainerWrap").hide();
     $("#patient-add").hide();
     $("#vprasanja").show();
+    $("#master-detail").show();
 }
 
 
@@ -231,17 +391,14 @@ function kreirajEHRuporabnika(pacient) {
                 if (party.action == 'CREATE') {
                     var patient = {ime: partyData.firstNames, priimek: partyData.lastNames, datumRojstva: partyData.dateOfBirth, id: ehrId};
                     myPatients.push(patient);
-                    drawDropdownMenu(myPatients);
+                    getPacients();
                     drawAlertDiv("success", "Uspešno generirani pacienti!");
-                    drawPatients(myPatients);
-                    kreiranjeUspelo = true;
-                    console.log(ehrId);
+                    dodajVitalneZnakePacientu(ehrId);
                 }
                 
             },
             error: function(err) {
                 drawAlertDiv("danger", JSON.parse(err.responseText).userMessage + "'!")
-                kreiranjeUspelo = false;
             }
         });
     }
@@ -251,17 +408,17 @@ function kreirajEHRuporabnika(pacient) {
         url: baseUrl + "/ehr",
 	    type: 'POST',
 	    success: function (data) {
-		        var ehrId = data.ehrId;
-		        var partyData = {
-		            firstNames: pacient.ime,
-		            lastNames: pacient.priimek,
-		            dateOfBirth: pacient.datumRojstva,
-		            partyAdditionalInfo: [
-		                {key: "ehrId", value: ehrId},
-		                {key: "app_id", value: "eZdravnik"}
-		            ]
-		        };
-		        stepTwo(partyData, ehrId);
+	        var ehrId = data.ehrId;
+	        var partyData = {
+	            firstNames: pacient.ime,
+	            lastNames: pacient.priimek,
+	            dateOfBirth: pacient.datumRojstva,
+	            partyAdditionalInfo: [
+	                {key: "ehrId", value: ehrId},
+	                {key: "app_id", value: "eZdravnikBase"}
+	            ]
+	        };
+	        stepTwo(partyData, ehrId);
         }
     });
 }
