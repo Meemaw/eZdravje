35a64ce00ceaa911436804f3bacd998658ef4bdc
Matej <totalfr33k@gmail.com>
2016-06-03 15:15:00 +0000

    Dodan graf telesne temperature

--- knjiznice/js/koda.js
+++ knjiznice/js/koda.js
@@ -226,6 +226,7 @@ function pacientClick(pacient) {
     preberiVitalneZnake(pacient.id, drawHeight, "height");
     preberiVitalneZnake(pacient.id, drawBloodPressure, "blood_pressure");
     preberiVitalneZnake(pacient.id, drawO2, "spO2");
+    
 }
 
 function drawSymptoms(result) {
@@ -286,6 +287,7 @@ function drawHeight(results) {
 }
 
 function drawTemperatureChart(results) {
+    izrisiGraf(results);
 }
 
 function drawWeight(results) {
@@ -332,6 +334,63 @@ function setSessionId() {
     $.ajaxSetup({
 		    headers: {"Ehr-Session": sessionId}
 	});
+}
+
+
+function izrisiGraf(data) {
+
+    $("#temperatureChart").empty();
+    $("#temperatureChart").append('<h4 class="text-name">Graf telesne temperature</h4>');
+
+    var margin = {top: 20, right: 20, bottom: 30, left: 40},
+    width = 600 - margin.left - margin.right,
+    height = 300 - margin.top - margin.bottom;
+
+    var x = d3.scale.ordinal()
+        .rangeRoundBands([0, width], .1);
+    
+    var y = d3.scale.linear()
+        .range([height, 0]);
+    
+    var xAxis = d3.svg.axis()
+        .scale(x)
+        .orient("bottom");
+    
+    var yAxis = d3.svg.axis()
+        .scale(y)
+        .orient("left");
+    
+    var svg = d3.select("#temperatureChart").append("svg")
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom)
+      .append("g")
+        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+    
+      x.domain(data.map(function(d) { return parseDate(d.time); }));
+      y.domain([0, d3.max(data, function(d) { return d.temperature; })]);
+    
+      svg.append("g")
+          .attr("class", "x axis")
+          .attr("transform", "translate(0," + height + ")")
+          .call(xAxis);
+    
+      svg.append("g")
+          .attr("class", "y axis")
+          .call(yAxis)
+        .append("text")
+          .attr("transform", "rotate(-90)")
+          .attr("y", 6)
+          .attr("dy", ".71em")
+          .style("text-anchor", "end");
+    
+      svg.selectAll(".bar")
+          .data(data)
+        .enter().append("rect")
+          .attr("class", "bar")
+          .attr("x", function(d) { return x(parseDate(d.time)); })
+          .attr("width", x.rangeBand())
+          .attr("y", function(d) { return y(d.temperature); })
+          .attr("height", function(d) { return height - y(d.temperature); });
 }
 
 
